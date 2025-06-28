a78a99c84315a3c9196956fd048996c057378ff8
pixel <pixelisprocrastinating15@gmail.com>
2025-06-28 10:32:50 +0200

    Update routes.js

--- js/routes.js
+++ js/routes.js
@@ -1,9 +1,11 @@
 import List from './pages/List.js';
+import List from './pages/SpamList.js';
 import Leaderboard from './pages/Leaderboard.js';
 import Roulette from './pages/Roulette.js';
 
 export default [
     { path: '/', component: List },
+    { path: '/spamlist', component: Spam List },
     { path: '/leaderboard', component: Leaderboard },
     { path: '/roulette', component: Roulette },
 ];
